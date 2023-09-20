b81be97debcd4248bdbbf5606660a48a70df2246
Advaith Reddy <83079702+advaithreddy@users.noreply.github.com>
2023-09-20 16:42:30 +0530

    Added H index

--- TopInterview150/ArrayString/hindex.cpp
+++ TopInterview150/ArrayString/hindex.cpp
@@ -29,11 +29,29 @@ int solve(vector<int> &a, int n){
     return n;
 }
 
+// H index is similar but here default the array is given in sorted manner
+int hindex2(vector<int> &a,int n){
+    // This method uses binary search
+    int low = 0, high =n-1;
+    int ans = 0;
+    while(low <= high){
+        int mid = low + ((high - low) >> 1);
+        int max_cit = n-mid;
+        if(a[mid] >= max_cit){
+            ans = max_cit;
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return ans;
+}
+
 signed main(){
 
-    vector<int> a = {3,0,6,1,5};
+    vector<int> a = {1,1,3};
     int n = a.size();
-    cout<<"The H- Index is "<<solve(a,n);
+    cout<<"The H- Index is "<<hindex2(a,n);
 
     return 0;
 }
